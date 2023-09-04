b39d6400d484d2c6116ea36fbf68c5823bba1ea0
DESKTOP-HMP8CP3\luand <luan@usezapay.com.br>
2023-09-04 20:33:43 -0300

    colocando o firebase no app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
-import 'package:flutter/material.dart' show runApp;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'app/app_module.dart';
 import 'app/app_widget.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(
     ModularApp(
       module: AppModule(),
